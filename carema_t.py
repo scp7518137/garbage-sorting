da3b74bff3e4a8022650164d0b1121d5f7298214
scp7518137 <122028745+scp7518137@users.noreply.github.com>
2024-10-19 15:12:31 +0800

    摄像头测试代码

---
+++ carema_t.py
@@ -0,0 +1,29 @@
+import cv2
+
+# 选择视频源，0通常是默认的内置或第一个连接的摄像头
+cap = cv2.VideoCapture(14)
+
+# 检查摄像头是否成功打开
+if not cap.isOpened():
+    print("无法打开摄像头")
+    exit()
+
+while True:
+    # 读取一帧图像
+    ret, frame = cap.read()
+
+    # 如果正确读取帧，ret将为True
+    if not ret:
+        print("无法接收帧 (可能已达到视频结尾？)。退出...")
+        break
+
+    # 显示结果帧
+    cv2.imshow('Camera Feed', frame)
+
+    # 按下'q'键退出循环
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 当一切完成后，释放VideoCapture对象
+cap.release()
+cv2.destroyAllWindows()
--- run.py
+++ run.py
@@ -9,36 +9,11 @@ pi = pigpio.pi()
 
 i = 0
 
-servoPin1 = 1  # x轴舵机
-servoPin2 = 2  # y轴舵机
-servoPin3 = 3  # 东舵机
-servoPin4 = 4  # 南舵机
-servoPin5 = 5  # 西舵机
-servoPin6 = 6  # 北舵机
-servoPin7 = 7  # 中舵机
-servoPin8 = 8  # 夹舵机
-servoPin9 = 9
-servoPin10 = 10
-servoPin11 = 11
-
 frequency = 50
 
 zero = 750     # 微秒，对应0度
 oh_eighty = 2250    # 微秒，对应180度
 ninety = 1500   # 微秒，对应90度
-
-pi.set_servo_pulsewidth(servoPin1, zero)
-pi.set_servo_pulsewidth(servoPin2, zero)
-pi.set_servo_pulsewidth(servoPin3, zero)
-pi.set_servo_pulsewidth(servoPin4, zero)
-pi.set_servo_pulsewidth(servoPin5, zero)
-pi.set_servo_pulsewidth(servoPin6, zero)
-pi.set_servo_pulsewidth(servoPin7, zero)
-pi.set_servo_pulsewidth(servoPin8, zero)
-pi.set_servo_pulsewidth(servoPin9, zero)
-pi.set_servo_pulsewidth(servoPin10, zero)
-pi.set_servo_pulsewidth(servoPin11, zero)
-
 
 # buffer_str = []   # 创建缓存区
 model_path1 = '/home/classify/garbage-sorting/model/input/best.h5'  # 输入加载模型绝对路径
