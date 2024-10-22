cea6234f23750a38176ce2917a5ffee9c4b528f5
scp7518137 <122028745+scp7518137@users.noreply.github.com>
2024-10-22 14:41:17 +0800

    修改了server

--- servo.py
+++ servo.py
@@ -23,102 +23,103 @@ b = [1, 1100, 2, 950, 3, 1100, 4, 950]
 
 #  下面4放下
 ServoControl.setPWMServoMoveByArray(a, 4, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 #  中心舵机归零
 ServoControl.setPWMServoMove(5, 1500, 500)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMove(5, 1300, 500)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMove(5, 1500, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 #  夹子归零
 ServoControl.setPWMServoMove(6, cl, 500)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMove(6, ct, 500)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMove(6, cl, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 # 旋转归零
 ServoControl.setPWMServoMove(7, 750, 1000)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMove(7, 1500, 1000)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMove(7, 750, 1000)
-time.sleep(0.8)
+time.sleep(1.5)
 
 # 180抬起
 ServoControl.setPWMServoMoveByArray(b, 4, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 # 360舵机移动
 ServoControl.setPWMServoMoveByArray(spl, 2, 500)
 time.sleep(3)
 ServoControl.setPWMServoMoveByArray(stlr, 2, 500)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMoveByArray(spa, 2, 500)
 time.sleep(3)
 ServoControl.setPWMServoMoveByArray(stfa, 2, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 #  夹子松开
 ServoControl.setPWMServoMove(6, cl, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 #  旋转舵机
 ServoControl.setPWMServoMove(7, 1000, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 #  夹子加紧
 ServoControl.setPWMServoMove(6, ct, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 # 360舵机移动
 ServoControl.setPWMServoMoveByArray(spl, 2, 500)
 time.sleep(3)
 ServoControl.setPWMServoMoveByArray(stlr, 2, 500)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMoveByArray(spf, 2, 500)
 time.sleep(3)
 ServoControl.setPWMServoMoveByArray(stfa, 2, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
-#  放下12
+#  放下12舵机
 ServoControl.setPWMServoMove(1, al, 500)
-time.sleep(0.8)
+time.sleep(1)
 ServoControl.setPWMServoMove(2, al, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 #  夹子松开
 ServoControl.setPWMServoMove(6, cl, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 #  移动归零
 ServoControl.setPWMServoMoveByArray(spr, 2, 500)
-time.sleep(6)
+time.sleep(1)
 ServoControl.setPWMServoMoveByArray(stlr, 2, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 #  旋转归零
-ServoControl.setPWMServoMove(7, 750, 1000)
-time.sleep(1)
+ServoControl.setPWMServoMove(7, 750, 500)
+time.sleep(1.5)
 
 #  180舵机归零
 ServoControl.setPWMServoMoveByArray(b, 4, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 #  180舵机放下
 ServoControl.setPWMServoMoveByArray(a, 4, 500)
-time.sleep(0.8)
+time.sleep(1.5)
 
 # 地盘倾斜
-ServoControl.setPWMServoMove(5, 1300, 1000)
+ServoControl.setPWMServoMove(5, 1300, 500)
 time.sleep(1)
-ServoControl.setPWMServoMove(5, 1500, 1000)
+ServoControl.setPWMServoMove(5, 1500, 500)
 time.sleep(1)
-ServoControl.setPWMServoMove(5, 1300, 1000)
-time.sleep(1)
+ServoControl.setPWMServoMove(5, 1300, 500)
+time.sleep(1.5)
 
 #  180升高
 ServoControl.setPWMServoMoveByArray(b, 4, 500)
 time.sleep(0.8)
+
