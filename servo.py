c30ab1ffeb9135bb4dfd5695da02aacf72db2c03
scp7518137 <122028745+scp7518137@users.noreply.github.com>
2024-10-22 13:10:31 +0800

    111

--- servo.py
+++ servo.py
@@ -11,99 +11,106 @@ bi = 1300  # 最低电机倾斜位
 cl = 750  # 爪子松开位
 ct = 1450  # 爪子紧张位
 
+
+spl = [9, 1000, 11, 2000]
+spr = [9, 2000, 11, 1000]
+spf = [8, 1000, 10, 2000]
+spa = [8, 2000, 10, 1000]
+stfa = [8, 1500, 10, 1500]
+stlr = [9, 1500, 11, 1500]
+a = [1, 800, 2, 800, 3, 800, 4, 800]
+
 #  下面五个舵机归零
-ServoControl.setBusServoMove(1, al, 1000)
-ServoControl.setBusServoMove(2, al, 1000)
-ServoControl.setBusServoMove(3, al, 1000)
-ServoControl.setBusServoMove(4, al, 1000)
-time.sleep(1)
+ServoControl.setPWMServoMoveByArray(a, 4, 1000)
+time.sleep(1.5)
+
 
 #  夹子舵机归零，旋转舵机归零
-ServoControl.setBusServoMove(6, cl, 1000)
-ServoControl.setBusServoMove(7, 750, 1000)
+ServoControl.setPWMServoMove(6, cl, 1000)
+ServoControl.setPWMServoMove(7, 750, 1000)
 time.sleep(1)
-ServoControl.setBusServoMove(6, ct, 1000)
-ServoControl.setBusServoMove(7, 1500, 1000)
+ServoControl.setPWMServoMove(6, ct, 1000)
+ServoControl.setPWMServoMove(7, 1500, 1000)
 time.sleep(1)
-ServoControl.setBusServoMove(6, cl, 1000)
-ServoControl.setBusServoMove(7, 750, 1000)
-time.sleep(1)
-
-ServoControl.setBusServoMove(5, 1500, 1000)
-time.sleep(1)
-ServoControl.setBusServoMove(5, 1300, 1000)
-time.sleep(1)
-ServoControl.setBusServoMove(5, 1500, 1000)
+ServoControl.setPWMServoMove(6, cl, 1000)
+ServoControl.setPWMServoMove(7, 750, 1000)
 time.sleep(1)
 
-ServoControl.setBusServoMove(1, a1h, 1000)
-ServoControl.setBusServoMove(2, a2h, 1000)
-ServoControl.setBusServoMove(3, a1h, 1000)
-ServoControl.setBusServoMove(4, a2h, 1000)
+ServoControl.setPWMServoMove(5, 1500, 1000)
+time.sleep(1)
+ServoControl.setPWMServoMove(5, 1300, 1000)
+time.sleep(1)
+ServoControl.setPWMServoMove(5, 1500, 1000)
+time.sleep(1)
+
+ServoControl.setPWMServoMove(1, a1h, 1000)
+ServoControl.setPWMServoMove(2, a2h, 1000)
+ServoControl.setPWMServoMove(3, a1h, 1000)
+ServoControl.setPWMServoMove(4, a2h, 1000)
 
 # 360舵机移动
-ServoControl.setBusServoMove(8, 1100, 1000)
-ServoControl.setBusServoMove(10, 1100, 1000)
+ServoControl.setPWMServoMove(8, 1100, 1000)
+ServoControl.setPWMServoMove(10, 1100, 1000)
 time.sleep(1)
-ServoControl.setBusServoMove(9, 1100, 1000)
-ServoControl.setBusServoMove(11, 1100, 1000)
+ServoControl.setPWMServoMove(9, 1100, 1000)
+ServoControl.setPWMServoMove(11, 1100, 1000)
 time.sleep(1)
 
 #  夹子松开
-ServoControl.setBusServoMove(6, cl, 1000)
+ServoControl.setPWMServoMove(6, cl, 1000)
 time.sleep(1)
 #  旋转舵机
-ServoControl.setBusServoMove(7, 1000, 1000)
+ServoControl.setPWMServoMove(7, 1000, 1000)
 time.sleep(1)
 #  夹子加紧
-ServoControl.setBusServoMove(6, ct, 1000)
+ServoControl.setPWMServoMove(6, ct, 1000)
 
 # 360舵机移动
-ServoControl.setBusServoMove(8, 1100, 1000)
-ServoControl.setBusServoMove(10, 1100, 1000)
+ServoControl.setPWMServoMove(8, 1100, 1000)
+ServoControl.setPWMServoMove(10, 1100, 1000)
 time.sleep(1)
-ServoControl.setBusServoMove(9, 1100, 1000)
-ServoControl.setBusServoMove(11, 1100, 1000)
+ServoControl.setPWMServoMove(9, 1100, 1000)
+ServoControl.setPWMServoMove(11, 1100, 1000)
 time.sleep(1)
 
 #  放下12
-ServoControl.setBusServoMove(1, al, 1000)
-ServoControl.setBusServoMove(2, al, 1000)
+ServoControl.setPWMServoMove(1, al, 1000)
+ServoControl.setPWMServoMove(2, al, 1000)
 
 #  夹子松开
-ServoControl.setBusServoMove(6, cl, 1000)
+ServoControl.setPWMServoMove(6, cl, 1000)
 time.sleep(1)
 
 #  移动归零
-ServoControl.setBusServoMove(8, 1100, 1000)
-ServoControl.setBusServoMove(10, 1100, 1000)
+ServoControl.setPWMServoMove(8, 1100, 1000)
+ServoControl.setPWMServoMove(10, 1100, 1000)
 time.sleep(1)
-ServoControl.setBusServoMove(9, 1100, 1000)
-ServoControl.setBusServoMove(11, 1100, 1000)
+ServoControl.setPWMServoMove(9, 1100, 1000)
+ServoControl.setPWMServoMove(11, 1100, 1000)
 time.sleep(1)
 #  旋转归零
-ServoControl.setBusServoMove(7, 750, 1000)
+ServoControl.setPWMServoMove(7, 750, 1000)
 time.sleep(1)
 #  180舵机归零
-ServoControl.setBusServoMove(1, 1100, 1000)
-ServoControl.setBusServoMove(2, 950, 1000)
+ServoControl.setPWMServoMove(1, 1100, 1000)
+ServoControl.setPWMServoMove(2, 950, 1000)
 time.sleep(2)
 
 #  180舵机放下
-ServoControl.setBusServoMove(1, al, 1000)
-ServoControl.setBusServoMove(2, al, 1000)
-ServoControl.setBusServoMove(3, al, 1000)
-ServoControl.setBusServoMove(4, al, 1000)
+ServoControl.setPWMServoMove(1, al, 1000)
+ServoControl.setPWMServoMove(2, al, 1000)
+ServoControl.setPWMServoMove(3, al, 1000)
+ServoControl.setPWMServoMove(4, al, 1000)
 time.sleep(1)
 # 地盘倾斜
-ServoControl.setBusServoMove(5, 1300, 1000)
+ServoControl.setPWMServoMove(5, 1300, 1000)
 time.sleep(1)
-ServoControl.setBusServoMove(5, 1500, 1000)
+ServoControl.setPWMServoMove(5, 1500, 1000)
 time.sleep(1)
-ServoControl.setBusServoMove(5, 1300, 1000)
+ServoControl.setPWMServoMove(5, 1300, 1000)
 time.sleep(1)
 #  180升高
-ServoControl.setBusServoMove(1, a1h, 1000)
-ServoControl.setBusServoMove(2, a2h, 1000)
-ServoControl.setBusServoMove(3, a1h, 1000)
-ServoControl.setBusServoMove(4, a2h, 1000)
+ServoControl.setPWMServoMove(1, a1h, 1000)
+ServoControl.setPWMServoMove(2, a2h, 1000)
+ServoControl.setPWMServoMove(3, a1h, 1000)
+ServoControl.setPWMServoMove(4, a2h, 1000)
--- t.py
+++ t.py
@@ -1,9 +1,25 @@
 import time
 import pigpio
+import serial
+import ServoControl
 
-pi = pigpio.pi()
-
-gpio17 = 17
-pi.set_mode(gpio17,pigpio.OUTPUT)
-
-pi.write(gpio17,1)
+a = [1, 800, 2, 800, 3, 800, 4, 800]
+b = [1, 1100, 2, 1100, 3, 1100, 4, 1100]
+ServoControl.setPWMServoMoveByArray(a, 4, 1000)
+time.sleep(2)
+ServoControl.setPWMServoMoveByArray(b, 4, 1000)
+#ServoControl.setPWMServoMove(1, 800, 1000)
+#ServoControl.setPWMServoMove(2, 800, 1000)
+#ServoControl.setPWMServoMove(3, 800, 1000)
+#ServoControl.setPWMServoMove(4, 800, 1000)
+#spl = [9,1000,11,2000]
+#spr = [9,2000,11,1000]
+#spf = [8,1000,10,2000]
+#spa = [8,2000,10,1000]
+#stfa = [8,1500,10,1500]
+#stlr = [9,1500,11,1500]
+#ServoControl.setPWMServoMoveByArray(spa,2,100)
+#time.sleep(1)
+#ServoControl.setPWMServoMoveByArray(stfa,2,100)
+#time.sleep(1)
+#ServoControl.setPWMServoMoveByArray(spl,2,100)
